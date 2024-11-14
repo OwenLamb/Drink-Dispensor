f289f679d34c7560094f67b271c25da2a546fd56
OwenLamb <70539710+OwenLamb@users.noreply.github.com>
2024-11-14 10:57:40 -0500

    Update DrinkDispensor.ino
    Adding ultra sonic sensor code.

--- DrinkDispensor.ino
+++ DrinkDispensor.ino
@@ -1,6 +1,11 @@
+#include "SR04.h"
+#define trigPin 12
+#define echoPin 11
+SR04 ultrasonicSensor = SR04(echoPin, trigPin);
+
+
 const int startButtonPin = 2;
 const int ledPin = 3;
-const int enaPin = 11;
 const int LAin1Pin = 10;
 const int LAin2Pin = 9;
 const int Relay1Pin = 7;
@@ -8,8 +13,19 @@ const int Relay2Pin = 6;
 const int resetButtonPin = 4;
 const int stopButtonPin = 5;
 
+unsigned long startTime = -44700;
+unsigned long resetTime = -44700;
+unsigned long blinkTime = 0;
+unsigned long checkDistance;
+
+int startButton;
+bool startFunction = false;
+int resetButton;
+bool resetFunction = false;
+int stopButton;
+int ledState = HIGH;
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(startButtonPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   pinMode(LAin1Pin, OUTPUT);
@@ -22,94 +38,94 @@ void setup() {
   digitalWrite(LAin1Pin, LOW);
   digitalWrite(LAin2Pin, HIGH);
 
-  //digitalWrite(11, HIGH);
   Serial.begin(9600);
 }
-unsigned long startTime = -32200;
-unsigned long resetTime = -32200;
-unsigned long stopTime = 0;
-
-int startButton;
-bool startFunction = false;
-int resetButton;
-bool resetFunction = false;
-int stopButton;
-bool trip1 = true;
-
 
 void loop() {
   startButton = digitalRead(startButtonPin);
   resetButton = digitalRead(resetButtonPin);
   stopButton = digitalRead(stopButtonPin);
-
-  if ((startButton == LOW) && (millis() - startTime) > 32200) {
-    Serial.println("I AM HERE");
+  if ((startButton == LOW) && (millis() - startTime) > 44700) {
     startTime = millis();
-    Serial.println(millis());
-    Serial.println(startTime);
-
     startFunction = true;
   }
 
-  if (resetButton == LOW && (millis() - resetTime) > 12500) {
+  if (resetButton == LOW && (millis() - resetTime) > 20000) {
     resetTime = millis();
     resetFunction = true;
   }
 
   if (stopButton == LOW) {
-    startTime = -32200;
-    resetTime = -32200;
-    //stop all pumps and lower actuator
+    startFunction = false;
+    resetFunction = false;
+    startTime = -44700;
+    resetTime = -44700;
+    // Stop all pumps and lower actuator
     digitalWrite(Relay2Pin, LOW);
     digitalWrite(Relay1Pin, LOW);
     digitalWrite(LAin1Pin, HIGH);
     digitalWrite(LAin2Pin, LOW);
   }
 
-  //Lower cup and fill drink
-  if (startFunction == true && (millis() - startTime) <= 32200) {
-    //Stage one lower actuator
-    if (trip1) {
-      Serial.println("intrip");
+  //Safety measure, if there isn't a cup detected get out of the start function
+  if ((millis() - startTime) > 19900 && (millis() - startTime) < 24700) {
+    checkDistance = ultrasonicSensor.Distance();
+    //if nothing within 22 cm's is detected then set start to false and turn off pumps
+    if (checkDistance > 22) {
+      startFunction = false;
+      digitalWrite(Relay1Pin, LOW);
+      digitalWrite(Relay2Pin, LOW);
+
+    }
+  }
+
+  // Lower cup and fill drink if the button is pressed, within time frame, and the reset button hasn't been pressed
+  if (startFunction == true && (millis() - startTime) <= 44700 && resetFunction == false) {
+    // Stage one: lower actuator
+    if ((millis() - startTime) <= 20000) {
       digitalWrite(ledPin, HIGH);
       digitalWrite(LAin1Pin, HIGH);
       digitalWrite(LAin2Pin, LOW);
-      trip1 = false;
     }
 
-    //Stage two turn on pump1 then off after 6 seconds
-    if ((millis() - startTime) > 12500 && (millis() - startTime) <= 18500) {
-      //Turn on pumps
+    // Stage two: turn on pump1 then off after 6 seconds
+    if ((millis() - startTime) > 20000 && (millis() - startTime) <= 23000) {
+      // Turn on pumps
       digitalWrite(Relay1Pin, HIGH);
-    } else if ((millis() - startTime) >= 18500) {
+    } else if ((millis() - startTime) >= 23000) {
       digitalWrite(Relay1Pin, LOW);
     }
 
-    //Stage three turn on pump2 same time as 1 but turn off after 7 seconds
-    if ((millis() - startTime) > 12500 && (millis() - startTime) <= 19500) {
+    // Stage three: turn on pump2 same time as 1 but turn off after 7 seconds
+    if ((millis() - startTime) > 20000 && (millis() - startTime) <= 24500) {
       digitalWrite(Relay2Pin, HIGH);
-    } else if ((millis() - startTime) > 19500) {
+    } else if ((millis() - startTime) > 24500) {
       digitalWrite(Relay2Pin, LOW);
     }
 
-    //Stage four raise actuator
-    if ((millis() - startTime) >= 19700) {
+    // Stage four: raise actuator
+    if ((millis() - startTime) >= 24700) {
       digitalWrite(LAin1Pin, LOW);
       digitalWrite(LAin2Pin, HIGH);
     }
-  } else {
+  } else if (startFunction) {
     digitalWrite(ledPin, LOW);
-    startFunction == false;
-    trip1 = true;
+    startFunction = false;
   }
 
-  //Reset actuator position
-  if ((millis() - resetTime) <= 12500) {
-    Serial.println("In reset");
+  // Reset actuator position
+  if (resetFunction == true && (millis() - resetTime) <= 20000) {
     digitalWrite(LAin1Pin, LOW);
     digitalWrite(LAin2Pin, HIGH);
-    //if stop is initiated or the actuator is done rising
-  } else if (resetFunction == true) {
-    resetFunction == false;
+
+    // Blink LED every half second
+    if ((millis() - blinkTime) >= 500) {
+      digitalWrite(ledPin, ledState);
+      ledState = !ledState;
+      blinkTime = millis();
+    }
+  } else {
+    resetFunction = false;
+    digitalWrite(ledPin, LOW);
   }
 }
